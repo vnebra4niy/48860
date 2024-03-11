51dca62d2e1e06e9d18a9b4bdebe7ad49b52f858
vnebra4niy <119805901+vnebra4niy@users.noreply.github.com>
2024-03-11 20:00:13 +0100

    Update student.js

--- student.js
+++ student.js
@@ -6,4 +6,7 @@ const STUDENT_ID = 333444;
 const getStudentFullName = () => FULLNAME;
 const getStudentId = () => STUDENT_ID;
 
-console.log(`My name is ${getStudentFullName()}. My student ID is ${getStudentId()}`);
+module.exports = {
+  getStudentFullName,
+  getStudentId,
+};
