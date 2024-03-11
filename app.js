76ba4f94476ac7f402afa581afc2862fc0dd3d92
vnebra4niy <119805901+vnebra4niy@users.noreply.github.com>
2024-03-11 20:40:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
 // app.js
 
-const student = require('./student');
-
 const http = require('http');
+const student = require('./student');
 
 const PORT = 3000;
 
 const server = http.createServer(requestListener);
 
 function requestListener(req, res) {
-  res.setHeader('Content-Type', 'text/html');
-  res.end(`
-    <html>
-      <head><title>About student</title></head>
-      <body>
-        <p>My name is ${student.getStudentFullName()}. My student ID is ${student.getStudentId()}</p>
-      </body>
-    </html>
-  `);
+  console.log(`Server is running on ${PORT}`);
+  console.log(`My name is ${student.getStudentFullName()}. My student ID is ${student.getStudentId()}`);
+  res.end();
 }
 
 server.listen(PORT, () => {
